69e6d82d111715eefd861f8ecfe120cb35e7022a
Charles Renan Retes <retescharlesrenan@gmail.com>
2024-06-16 17:36:06 +0800

    S - Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import { useState } from "react";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 export function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  );
 }
 export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div>
+      <img src="" alt="" />
+      <div>
+        <div>
+          <h1>Itori</h1>
+          <h2>“Where Every Sip Tells a Story”</h2>
+          <div></div>
+          <p>
+            Welcome to Itori, where every cup of coffee tells a story. Immerse
+            yourself in the rich aromas and cozy ambiance of our shop, your new
+            favorite spot to relax and unwind.
+          </p>
+          <button>Order Now</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
