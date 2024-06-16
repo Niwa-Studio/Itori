214a1612f2b69de065b00eba8fe6189a32e0b588
Charles Renan Retes <retescharlesrenan@gmail.com>
2024-06-16 17:26:55 +0800

    D - Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import "./App.css";
+import Header from "./components/Header";
 
 export function App() {
-  return;
+  return <Header />;
 }
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import logoImage from "../assets/icons/logo-image.png";
+import coffee from "../assets/icons/coffee.svg";
+import enter from "../assets/icons/enter.svg";
+import help from "../assets/icons/help.svg";
+
+const Header = () => {
+  return (
+    <header className="mx-auto flex justify-between pt-10 lg:container">
+      <div className="flex items-center gap-5">
+        <img src={logoImage} alt="" />
+        <h1 className="text-4xl font-semibold">ITORI</h1>
+      </div>
+      <nav className="flex items-center">
+        <div className="flex gap-5 px-5">
+          <img src={coffee} alt="" />
+          <a href="/">Products</a>
+        </div>
+        <div className="flex gap-5 px-5">
+          <img src={enter} alt="" />
+          <a href="/">Login</a>
+        </div>
+        <div className="flex gap-5 px-5">
+          <img src={help} alt="" />
+          <a href="/">About</a>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
