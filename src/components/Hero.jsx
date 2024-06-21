ff5d868667830142dd2d9d14bc7eceedd9bd1928
Charles Renan Retes <retescharlesrenan@gmail.com>
2024-06-21 20:51:35 +0800

    D - Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,24 +3,26 @@ import image from "../assets/images/homepage-coffee-pic.jpg";
 
 const Hero = () => {
   return (
-    <div className="mx-auto mb-40 mt-16 flex gap-16 lg:container">
+    <div className="mb-40 mt-16 flex gap-16">
       <div className="h-[624px] w-3/5">
         <img src={image} alt="" className="h-full w-full object-cover" />
       </div>
-      <div className="flex basis-[40%] flex-col gap-10">
-        <div>
-          <h1 className="text-7xl font-semibold">Itori</h1>
-          <h2 className="text-3xl italic">“Where Every Sip Tells a Story”</h2>
+      <div className="basis-[40%]">
+        <div className="flex w-10/12 flex-col gap-10">
+          <div>
+            <h1 className="text-7xl font-semibold">Itori</h1>
+            <h2 className="text-3xl italic">“Where Every Sip Tells a Story”</h2>
+          </div>
+          <div className="h-[2px] bg-black"></div>
+          <p>
+            Welcome to Itori, where every cup of coffee tells a story. Immerse
+            yourself in the rich aromas and cozy ambiance of our shop, your new
+            favorite spot to relax and unwind.
+          </p>
+          <button className="self-center rounded-full bg-shapes-tertiary px-12 py-4 text-white">
+            Order Now
+          </button>
         </div>
-        <div className="h-[2px] bg-black"></div>
-        <p>
-          Welcome to Itori, where every cup of coffee tells a story. Immerse
-          yourself in the rich aromas and cozy ambiance of our shop, your new
-          favorite spot to relax and unwind.
-        </p>
-        <button className="bg-shapes-tertiary self-center rounded-full px-12 py-4 text-white">
-          Order Now
-        </button>
       </div>
     </div>
   );
