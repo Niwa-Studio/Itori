24dff05976204ec349baed47acfabc801818e324
Charles Renan Retes <retescharlesrenan@gmail.com>
2024-06-22 13:21:57 +0800

    R - Reviews

---
+++ src/data/reviewsData.js
@@ -0,0 +1,23 @@
+const reviewsData = [
+  {
+    id: "Ra5wr",
+    title: "Best Coffee in Town!",
+    meessage:
+      "“I absolutely love Itori Coffee Shop! The ambiance is perfect for both working and relaxing. The baristas are incredibly friendly, and the coffee is always top-notch. Their latte art is a lovely touch, and the pastries are always fresh and delicious. I highly recommend trying their seasonal specials. Itori is my go-to spot for coffee“",
+    name: "Alex P.",
+  },
+  {
+    id: "kPcV4ERa5wr",
+    title: "A Cozy and Inviting Coffee Shop",
+    meessage:
+      "“Itori has quickly become my favorite place to unwind. The decor is cozy and inviting, making it the perfect place to read a book or catch up with friends. The menu has a great variety of drinks, and everything I've tried so far has been excellent. The matcha latte is my personal favorite. Plus, they have some of the best customer service I've experienced. Highly recommend!“",
+    name: "Jamie L.",
+  },
+  {
+    id: "65r5w53TRa5wr",
+    title: "A Hidden Gem!",
+    meessage:
+      "“Itori is a hidden gem in Brewville. The moment you walk in, you're greeted with the wonderful aroma of freshly brewed coffee. The staff is warm and welcoming, and they really know their stuff when it comes to coffee. I had the best cappuccino of my life here, and their cold brew is perfect for hot summer days. I'll definitely be coming back regularly.“",
+    name: "Taylor S.",
+  },
+];
