f5f6bf82a90296e3c2caa3a64dcf80783ecdba0b
Charles Renan Retes <retescharlesrenan@gmail.com>
2024-06-23 19:24:04 +0800

    S - imported all of the images to the productURLs.ks

--- src/assets/images/products/productURLs.js
+++ src/assets/images/products/productURLs.js
@@ -1,3 +1,25 @@
-for (let index = 0; index < 32; index++) {
-  console.log("import name from './name-img.jpeg'");
-}
+import americano from "./americano-img.jpg";
+import blacktea from "./black-tea-img.jpg";
+import brownies from "./brownies-img.jpg";
+import cappuccino from "./cappuccino-img.jpg";
+import capresepanini from "./caprese-panini-img.jpg";
+import chickencaesarwrap from "./chicken-caesar-wrap.jpg";
+import coldbrew from "./cold-brew-img.jpg";
+import cookies from "./cookies-img.jpg";
+import croissants from "./croissants-img.jpg";
+import espresso from "./espresso-img.jpg";
+import greendetoxjuice from "./green-detox-juice-img.jpg";
+import greentea from "./green-tea-img.jpg";
+import herbaltea from "./herbal-tea-img.jpg";
+import icedcoffee from "./iced-coffee-img.jpg";
+import icedlatte from "./iced-latte-img.jpg";
+import icedmocha from "./iced-mocha-img.jpg";
+import latte from "./latte-img.jpg";
+import macchiato from "./macchiato-img.jpg";
+import mangopineapplesmoothie from "./mango-pineapple-smoothie-img.jpg";
+import matchagreentea from "./matcha-green-tea-img.jpg";
+import muffins from "./muffins-img.jpg";
+import orangecarrotjuice from "./orange-carrot-juice-img.jpg";
+import strawberrybananasmoothie from "./strawberry-banana-smoothie-img.jpg";
+import turkeyavocadosandwich from "./turkey-avocado-sandwich-img.jpg";
+import veggiehummussandwhich from "./veggie-hummus-sandwhich-img.jpg";
