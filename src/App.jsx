9424bf3941e3cdfc6e429f509227e6a7b291c926
Charles Renan Retes <retescharlesrenan@gmail.com>
2024-06-23 12:01:55 +0800

    S - Routes for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,21 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
-import LandingPage from "./pages/LandingPage";
 import HFLayout from "./layouts/HFLayout";
+import LandingPage from "./pages/LandingPage";
+import ProductsPage from "./pages/ProductsPage";
+import OrdersPage from "./pages/OrdersPage";
+import AboutPage from "./pages/AboutPage";
+import SettingsPage from "./pages/SettingsPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<HFLayout />}>
       <Route index element={<LandingPage />} />
+      <Route path="/products" element={<ProductsPage />} />
+      <Route path="/orders" element={<OrdersPage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/settings" element={<SettingsPage />} />
     </Route>,
   ),
 );
