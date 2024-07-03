e435591adfd4a41c8926d993434bf3fcf139d69e
Charles Renan Retes <retescharlesrenan@gmail.com>
2024-07-03 19:48:24 +0800

    D - 2 Account details

--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -5,10 +5,12 @@ const Detail = ({ col1, col2 }) => {
   return (
     <div className="grid grid-cols-12">
       <span className="col-span-2 text-[18px]">{col1}</span>
-      <span className="col-span-8 col-start-4 text-[18px] text-[#6C6C6C]">
+      <span className="col-span-6 col-start-4 text-[18px] text-[#6C6C6C]">
         {col2}
       </span>
-      <img src={edit} alt="" className="justify-self-end" />
+      <div className="col-span-3 flex justify-end">
+        {col1 == "Password" ? <span>Reset Password</span> : <img src={edit} />}
+      </div>
     </div>
   );
 };
@@ -19,16 +21,22 @@ const SettingsPage = () => {
       <h1 className="mx-auto mb-10 text-3xl font-semibold lg:container">
         Settings
       </h1>
-      <div className="bg-shapes-grey-3">
+      <div className="bg-shapes-grey-3 py-10">
         <div className="mx-auto lg:container">
-          <h2 className="text-2xl font-semibold">Account</h2>
-          <div>
-            <Detail col1={"First Name"} col2={"Shino"} />
-            <Detail col1={"Last Name"} col2={"Yamamoto"} />
-            <Detail
-              col1={"Shipping Address"}
-              col2={"274 Sakura Blossom Lane, Moonlit Gardens, Serenity City"}
-            />
+          <h2 className="mb-6 text-2xl font-semibold">Account</h2>
+          <div className="flex flex-col gap-4">
+            <div className="flex flex-col gap-5 rounded-lg bg-shapes-grey-4 p-10">
+              <Detail col1={"First Name"} col2={"Shino"} />
+              <Detail col1={"Last Name"} col2={"Yamamoto"} />
+              <Detail
+                col1={"Shipping Address"}
+                col2={"274 Sakura Blossom Lane, Moonlit Gardens, Serenity City"}
+              />
+            </div>
+            <div className="flex flex-col gap-5 rounded-lg bg-shapes-grey-4 p-10">
+              <Detail col1={"Username"} col2={"shino45_"} />
+              <Detail col1={"Password"} col2={"********"} />
+            </div>
           </div>
         </div>
       </div>
